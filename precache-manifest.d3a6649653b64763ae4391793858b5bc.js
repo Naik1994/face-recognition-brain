70a678da253e2699e629a298d9f6099fad6b5677
Naik1994 <harishsnaik94@gmail.com>
2023-12-10 19:06:12 +0530

    Updates

---
+++ precache-manifest.d3a6649653b64763ae4391793858b5bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ebfcf18bbe15c592541ede238891a7",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "ef08bc1dcd196e29d5f3",
+    "url": "/face-recognition-brain/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "de61cd2391b82755e96d",
+    "url": "/face-recognition-brain/static/css/main.5145a602.chunk.css"
+  },
+  {
+    "revision": "ef08bc1dcd196e29d5f3",
+    "url": "/face-recognition-brain/static/js/2.a05d779d.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/face-recognition-brain/static/js/2.a05d779d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de61cd2391b82755e96d",
+    "url": "/face-recognition-brain/static/js/main.d4901c1a.chunk.js"
+  },
+  {
+    "revision": "33be454f861c9d52b30c",
+    "url": "/face-recognition-brain/static/js/runtime-main.d41efdb7.js"
+  }
+]);
